13e12144f1e15a07d1d9e9c461f11943f66e7418
arshedgithub <arshedahmedmail@gmail.com>
2022-07-05 20:31:52 +0530

    add loading text

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,5 +1,37 @@
+import React, { useEffect, useState } from "react";
+
 const Articles = () => {
-  return <h3>Articles</h3>;
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      setArticles(data);
+    }, 2000);
+  }, []);
+
+  return (
+    <div className="articles">
+      <h2>Articles</h2>
+
+      {articles &&
+        articles.map((article) => (
+          <div className="article" key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.title}</p>
+          </div>
+        ))}
+
+      {!articles && <p>Loading...</p>}
+    </div>
+  );
 };
 
 export default Articles;
+{
+  /* <h3>{article.title}</h3> */
+}
+{
+  /* <div>{article.body}</div> */
+}
--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,30 @@
+import React, { useEffect, useState } from "react";
+
 const User = () => {
-  return <h3>user detail</h3>;
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+      const data = await res.json();
+      setProfile(data);
+    }, 2000);
+  }, []);
+
+  return (
+    <div>
+      <h2>user details</h2>
+      {profile && (
+        <div className="user">
+          <h3>{profile.username}</h3>
+          <p>{profile.email}</p>
+          <a href={profile.website}> {profile.website}</a>
+        </div>
+      )}
+
+      {!profile && <p>Loading...</p>}
+    </div>
+  );
 };
 
 export default User;
